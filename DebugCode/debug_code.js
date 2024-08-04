72abed72e31a8d00a55c9537ead5483486d9fd81
Shuja2 <shuja201234@gmail.com>
2024-08-04 22:51:52 +0500

    Created Todo app

---
+++ DebugCode/debug_code.js
@@ -0,0 +1,51 @@
+
+function performOperation() {
+    // Get user input from input fields
+    let num1 = parseInt(document.getElementById('input1').value);
+    let num2 = parseInt(document.getElementById('input2').value);
+    // Check if inputs are valid numbers
+    if (!isNaN(num1) && !isNaN(num2)) {
+        // Perform the operation
+        let result = multiply(num1, num2);
+        // Display the result
+        displayResult(result);
+
+        //now sum up numbers
+        result = sum(num1, num2);
+        // Display the result
+        displayResult(result);
+
+        //now divide
+        result = divide(num1, num2);
+        // Display the result
+        displayResult(result);
+    } else {
+        displayResult('Please enter valid numbers');
+    }
+}
+
+function multiply(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+
+    // Multiply the numbers
+    return a * b;
+}
+
+function sum(a, b) {
+
+    // sum the numbers
+    return a + b;
+}
+
+function divide(a, b) {
+
+    // sum the numbers
+    return a / b;
+}
+
+function displayResult(result) {
+    // Display the result in the paragraph element
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `The result is: ${result}`;
+}
